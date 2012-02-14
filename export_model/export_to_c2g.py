bf634f408a96d9d1dacce7752dd5e3b70db69641
Vladimir Rutsky <altsysrq@gmail.com>
2012-02-14 07:04:44 +0400

    export vertex position and normal

--- export_model/export_to_c2g.py
+++ export_model/export_to_c2g.py
@@ -1,5 +1,7 @@
 # <pep8-80 compliant>
 
+import struct
+
 import bpy
 
 # ExportHelper is a helper class, defines filename and
@@ -8,11 +10,77 @@ from bpy_extras.io_utils import ExportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
 
 
-def export_to_c2g(context, filepath, use_some_setting, convert_quads_to_tris):
-    print("running write_some_data...")
-    f = open(filepath, 'w')
-    #f.write("Hello World %s" % use_some_setting)
-    f.close()
+MAGIC = b'CGSG'
+
+def object_data(obj, convert_quads_to_tris):
+    vertices = []
+    for v in obj.data.vertices:
+        vertices.append(list(v.co) + list(v.normal))
+
+    indices = []
+    for f in obj.data.faces:
+        if len(f.vertices) == 3:
+            indices.extend(list(f.vertices))
+        elif len(f.vertices) == 4 and convert_quads_to_tris:
+            indices.extend([f.vertices[0], f.vertices[1], f.vertices[2]])
+            indices.extend([f.vertices[0], f.vertices[2], f.vertices[3]])
+        else:
+            pass
+
+    return vertices, indices
+
+def prepare_data_v1_0(vertices, indices):
+    # All numeric types stored in little-endian.
+
+    data = b''
+
+    # 4 bytes - magic
+    data += MAGIC
+    # 4 bytes - format version
+    data += b'0100'
+    # 2 bytes unsigned short - single vertex size in bytes
+    data += struct.pack('<H', 4 * 3 + 4 * 3)
+    # 4 bytes unsigned int - number of vertices
+    data += struct.pack('<I', len(vertices))
+    # 2 bytes unsigned short - single index size in bytes
+    data += struct.pack('<H', 2)
+    # 4 bytes unsigned int - number of indices
+    data += struct.pack('<I', len(indices))
+
+    # Vertices
+    for v in vertices:
+        for c in v:
+            data += struct.pack('<f', c)
+
+    # Indices
+    for i in indices:
+        data += struct.pack('<H', i)
+
+    return data
+
+
+def write_data(filepath, format_version, vertices, indices):
+    if format_version == '0100':
+        data = prepare_data_v1_0(vertices, indices)
+
+    with open(filepath, 'wb') as f:
+        f.write(data)
+        
+
+def export_to_c2g(context, filepath, format_version, convert_quads_to_tris):
+    print("Exporting to '{0}'...".format(filepath))
+
+    vertices, indices = [], []
+    for obj in context.selected_objects:
+        obj_vertices, obj_indices = object_data(obj, convert_quads_to_tris)
+
+        off = len(vertices)
+        vertices.extend(obj_vertices)
+        indices.extend([idx + off for idx in obj_indices])
+
+    write_data(filepath, format_version, vertices, indices)
+
+    print("Successfully exported!")
 
     return {'FINISHED'}
 
@@ -78,7 +146,7 @@ if __name__ == "__main__":
         #unregister()
         pass
 
-    # register() # COMMENTED FOR DEBUG
+    register() # COMMENTED FOR DEBUG
 
     # test call
     bpy.ops.export.c2g('INVOKE_DEFAULT')
