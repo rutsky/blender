4a9414bf71707b31cbfa2974b29eb532323cebe5
Vladimir Rutsky <altsysrq@gmail.com>
2012-02-20 10:16:43 +0400

    minor

--- export_model/export_to_c2g.py
+++ export_model/export_to_c2g.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2012  Vladimir Rutsky <altsysrq@gmail.com>
 
-# MIT License:
+# The MIT License
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
